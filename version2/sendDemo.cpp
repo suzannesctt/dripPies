5e5b620702274b52a13b150bcb339b4b26d3ade2
Suzanne Scott <suzannesctt@gmail.com>
2020-04-15 17:32:12 +1000

    moved send into sendDemo for blink script, and made more general send script to send one binary code

--- version2/sendDemo.cpp
+++ version2/sendDemo.cpp
@@ -1,5 +1,7 @@
 /*
-Usage: ./send <code> [pin]
+Test of 433 MHz transmitter - repeats sending two codes (on and off) twice
+Codes and protocol are hard-coded
+Usage: ./sendDemo [pin]
 pin is according to wiringPi numbering
 */
 
@@ -7,13 +9,15 @@ pin is according to wiringPi numbering
 #include "rc-switch/RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
-	// check argc
-        if ((argc != 2) && (argc != 3)){
+
+
+	// check argv
+        if (argc > 2){
                 printf("Sending 433 MHz remote plug control codes\n");
-                printf("Usage: %s <code> [pin]\n", argv[0]);
-		printf("code - binary code to transmit (without sync bit)\n");
+                printf("Usage: %s [pin]\n", argv[0]);
                 printf("pin - wiringPi pin number [0]\n");
                 return -1;
         }
@@ -21,13 +25,10 @@ int main(int argc, char * argv[]) {
 	// set up wiringPi
         if (wiringPiSetup () == -1) return 1;
 
-	// get code to transmit
-	char* code = argv[1];
-
         // get pin if it was set
         int PIN;
-        if (argc == 3) {
-                PIN = atoi(argv[2]);
+        if (argc == 2) {
+                PIN = atoi(argv[1]);
         }
         else {
                 PIN = 0;
@@ -43,6 +44,14 @@ int main(int argc, char * argv[]) {
 	mySwitch.setProtocol(customprotocol);
 
 	// transmit
-	mySwitch.send(code);
+	int REPEAT = 2;
+	int i = 0;
+	while (i < REPEAT) {
+		mySwitch.send("010110100110100100001111000001110");
+		sleep(1);
+		mySwitch.send("010110100110100100001110000001100");
+		sleep(1);
+		i++;
+	}
 	return 0;
 }
