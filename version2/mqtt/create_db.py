b42b631785e0f3b2cf97d70fba83c8c0edbedf5d
Suzanne Scott <suzannesctt@gmail.com>
2020-05-30 18:35:57 +1000

    added script to create sqlite3 database

---
+++ version2/mqtt/create_db.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+# connect to database
+conn = sqlite3.connect('../data/garden.db')
+
+# create a table for the temperature
+# dates should be in ISO 8601 format
+temp = """CREATE TABLE temp
+		(id INTEGER PRIMARY KEY,
+		 temp  REAL,
+		 timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'NOW', 'localtime'))
+		 )
+	"""
+
+# similar table for humidity
+humidity = """CREATE TABLE humidity 
+                (id INTEGER PRIMARY KEY,
+                 humidity  REAL,
+		 timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'NOW', 'localtime'))
+                 )
+        """
+
+# similar table for voltage
+voltage = """CREATE TABLE voltage 
+                (id INTEGER PRIMARY KEY,
+                 voltage  REAL,
+		 timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'NOW', 'localtime'))
+                 )
+        """
+
+# tank table has two columns - time in us between trigger and echo, and percentage full
+tank = """CREATE TABLE tank 
+                (id INTEGER PRIMARY KEY,
+                 ustime INTEGER,
+		 full_pc  REAL,
+		 timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'NOW', 'localtime'))
+		 )
+        """
+
+# watering table to keep track of when last watered and for how long
+water = """CREATE TABLE water
+		(id INTEGER PRIMARY KEY,
+		 timestamp_start DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'NOW', 'localtime')),
+		 duration_sec INTEGER
+		)
+	"""
+
+c = conn.cursor()
+
+c.execute(temp)
+c.execute(humidity)
+c.execute(voltage)
+c.execute(tank)
+c.execute(water)
+
+conn.commit()
+
+conn.close()
