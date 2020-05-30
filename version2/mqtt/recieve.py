a103580a1d1e8871dd8269f93b99a6e3a4a88439
Suzanne Scott <suzannesctt@gmail.com>
2020-05-30 18:35:44 +1000

    updated recieve script

--- version2/mqtt/recieve.py
+++ version2/mqtt/recieve.py
@@ -1,5 +1,17 @@
 import paho.mqtt.client as mqtt
 from datetime import datetime
+from sys import argv
+from time import sleep
+
+
+# constants for calculating fraction of the tank that's full
+vair = 343 # velocity of sound in air (m/s)
+vwater = 1500 # velocity of sound in water (m/s)
+d = 0.55 # total depth of tank (m)
+
+
+if argv[1] is None:
+    raise ValueError('Please provide a path to save files as a command line argument')
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -16,7 +28,24 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(datetime.today().strftime('%Y-%m-%d-%H:%M:%S')+": "+msg.topic+" "+str(msg.payload))
+    topic = msg.topic
+    message = msg.payload.decode('utf-8')
+    time = datetime.today().strftime('%H:%M:%S')
+    date = datetime.today().strftime('%Y-%m-%d')
+    # if the topic is usTime, convert to fraction of the tank that's full
+    if topic == "garden/usTime":
+        sec = int(message)/1000000
+        frac_full = (vair*vwater*sec - 2*d*vwater)/(2*d*vair - 2*d*vwater)
+        print(f"{date} {time} {msg.topic}: {message} ({frac_full*100}% full)")
+    else:
+        print(f"{date} {time} {msg.topic}: {message}")
+    filename = f"{argv[1]}/{date}_{msg.topic.split('/')[1]}.txt"
+    with open(filename, "a+") as handle:
+        if topic == "garden/usTime":
+            handle.write(f"{time}\t{message}\t{frac_full}\n")
+        else:
+            handle.write(f"{time}\t{message}\n")
+
 
 
 client = mqtt.Client()
